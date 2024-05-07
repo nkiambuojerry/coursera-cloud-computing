4ea4dbf690f76b9a0889525b87d847671d6bb929
Jerry Nkiambuo <nkiambuojerry98@gmail.com>
2024-05-07 15:53:00 +0100

    fdggs

--- module-07/main.tf
+++ module-07/main.tf
@@ -196,6 +196,7 @@ resource "aws_autoscaling_attachment" "example" {
   # Wait for lb to be running before attaching to asg
   depends_on  = [aws_lb.lb]
   autoscaling_group_name = var.asg-name
+  lb_target_group_arn = aws_lb_target_group.alb-lb-tg.arn
 }
 
 output "alb-lb-tg-arn" {
