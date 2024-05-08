66363f4df6c92b9b24692cf8585aaccc27f72514
Jerry Nkiambuo <nkiambuojerry98@gmail.com>
2024-05-08 02:47:32 +0100

    wewre

--- module-final/upload-images-to-s3.sh
+++ module-final/upload-images-to-s3.sh
@@ -4,7 +4,7 @@
 
 # S3 commands
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/s3/index.html
-# Upload illinoistech.png and rohit.jpg to bucket ${19}
+# Upload illinoistech.png and rohit.jpg to bucket ${13}
 
 if [ $# = 0 ]
 then
@@ -12,27 +12,27 @@ then
   echo "Execute: bash ./upload-images-to-s3.sh $(< ~/arguments.txt)"
   exit 1 
 else
-    echo "Uploading image: ./images/illinoistech.png to s3://${19}..."
-    aws s3 cp ./images/illinoistech.png s3://${19}
-    echo "Uploaded image: ./images/illinoistech.png to s3://${19}..."
+    echo "Uploading image: ./Users/user/Documents/coursera-cloud-computing/images/illinoistech.png://${13}..."
+    aws s3 cp ./Users/user/Documents/coursera-cloud-computing/images/illinoistech.png://${13}
+    echo "Uploaded image: ./Users/user/Documents/coursera-cloud-computing/images/illinoistech.png://${13}..."
 
-    echo "Uploading image: ./images/rohit.jpg to s3://${19}..."
-    aws s3 cp ./images/rohit.jpg s3://${19}
-    echo "Uploaded image: ./images/rohit.jpg to s3://${19}..."
+    echo "Uploading image: ./Users/user/Documents/coursera-cloud-computing/images/rohit.jpg://${13}..."
+    aws s3 cp ./Users/user/Documents/coursera-cloud-computing/images/rohit.jpg://${13}
+    echo "Uploaded image: ./Users/user/Documents/coursera-cloud-computing/images/rohit.jpg://${13}..."
 
-    echo "Listing content of bucket: s3://${19}..."
-    aws s3 ls s3://${19}
+    echo "Listing content of bucket: s3://${13}..."
+    aws s3 ls s3://${13}
 
     # Upload ranking.jpg and elevate.webp to bucket ${20}
-    echo "Uploading image: ./images/elevate.webp to s3://${20}..."
-    aws s3 cp ./images/elevate.webp s3://${20}
-    echo "Uploaded image: ./images/elevate.webp to s3://${20}..."
+    echo "Uploading image: ./Users/user/Documents/coursera-cloud-computing/images/elevate.webp to s3://${14}..."
+    aws s3 cp ./Users/user/Documents/coursera-cloud-computing/images/elevate.webp s3://${14}
+    echo "Uploaded image: ./Users/user/Documents/coursera-cloud-computing/images/elevate.webp to s3://${14}..."
 
-    echo "Uploading image: ./images/ranking.jpg to s3://${20}..."
-    aws s3 cp ./images/ranking.jpg s3://${20}
-    echo "Uploaded image: ./images/ranking.jpg to s3://${20}..."
+    echo "Uploading image: ./Users/user/Documents/coursera-cloud-computing/images/ranking.jpg://${14}..."
+    aws s3 cp ./Users/user/Documents/coursera-cloud-computing/images/ranking.jpg://${14}
+    echo "Uploaded image: ./Users/user/Documents/coursera-cloud-computing/images/ranking.jpg://${14}..."
 
-    echo "Listing content of bucket: s3://${20}..."
-    aws s3 ls s3://${20}
+    echo "Listing content of bucket: s3://${14}..."
+    aws s3 ls s3://${14}
 # End of if
 fi
